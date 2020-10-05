31f3aa692091f901632efd874fcecd956546e5db
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-10-05 23:00:47 +0200

    CHANGED: Section-3

---
+++ Section-3/Assets/Scripts/BridgeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour
+{
+
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("Open", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("Open", false);
+    }
+
+}
---
+++ Section-3/Assets/Scripts/DoorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator anim;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        anim.SetBool("isOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("isOpening", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
