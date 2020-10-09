c79c7492f1d1c7640ad6851801ee4e76f3340f31
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2020-10-09 08:39:01 +0200

    ADDED: Section-5

---
+++ Section-5/Assets/Scripts/Drive.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    float speed = 2.5f;
+    float rotationSpeed = 100.0f;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation > 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("direction", 1.0f);
+        }
+        else if (translation < 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("direction", -1.0f);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            anim.SetTrigger("isJumping");
+        }        
+    }
+}
---
+++ Section-5/Assets/Scripts/Swim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour
+{
+    float speed = 1.0f;
+    float rotationSpeed = 100.0f;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation > 0)
+        {
+            anim.SetBool("isSwimming", true);
+            anim.SetFloat("direction", 1.0f);
+        }
+        else if (translation < 0)
+        {
+            anim.SetBool("isSwimming", true);
+            anim.SetFloat("direction", -1.0f);
+        }
+        else
+        {
+            anim.SetBool("isSwimming", false);
+        }
+    }
+}
