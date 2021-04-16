da3689a434960c53915f1365a9ed0016825e053b
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2021-04-16 12:47:45 +0200

    ADDED: Section-9

---
+++ Section-9/Assets/AnswerTelephone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerTelephone : MonoBehaviour
+{
+    public GameObject character;
+    public GameObject anchor;
+    bool isWalkingTowards = false;
+    bool standingNear = false;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = character.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isWalkingTowards)
+        {
+            AutoWalkTowards();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        AnimLerp();
+    }
+
+    private void OnMouseDown()
+    {
+        if (!standingNear)
+        {
+            anim.SetFloat("speed", 1);
+            anim.SetBool("isWalking", true);
+            isWalkingTowards = true;
+            Control.controlledBy = this.gameObject;
+        }
+
+    }
+}
---
+++ Section-9/Assets/Control.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    Animator anim;
+    float speed = 1.0f;
+    float rotationSpeed = 100f;
+    float weight = 1f;
+
+    public static GameObject controlledBy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+
+        transform.Rotate(0, rotation, 0);
+
+        if(translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("speed", translation);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("speed", 0);
+        }
+
+    }
+}
